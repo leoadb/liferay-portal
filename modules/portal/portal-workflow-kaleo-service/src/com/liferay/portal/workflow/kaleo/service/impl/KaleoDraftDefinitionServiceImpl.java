37d56e7e4f4f8a53e33bc2e4027764c12ab7bea8
NorbertKocsis <norbert.kocsis@liferay.com>
2015-08-31 11:34:31 -0300

    LPS-55322 Ran buildService

---
+++ modules/portal/portal-workflow-kaleo-service/src/com/liferay/portal/workflow/kaleo/service/impl/KaleoDraftDefinitionServiceImpl.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.workflow.kaleo.service.impl;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.portal.workflow.kaleo.service.base.KaleoDraftDefinitionServiceBaseImpl;
+
+/**
+ * The implementation of the kaleo draft definition remote service.
+ *
+ * <p>
+ * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.portal.workflow.kaleo.service.KaleoDraftDefinitionService} interface.
+ *
+ * <p>
+ * This is a remote service. Methods of this service are expected to have security checks based on the propagated JAAS credentials because this service can be accessed remotely.
+ * </p>
+ *
+ * @author Brian Wing Shun Chan
+ * @see KaleoDraftDefinitionServiceBaseImpl
+ * @see com.liferay.portal.workflow.kaleo.service.KaleoDraftDefinitionServiceUtil
+ */
+@ProviderType
+public class KaleoDraftDefinitionServiceImpl
+	extends KaleoDraftDefinitionServiceBaseImpl {
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never reference this class directly. Always use {@link com.liferay.portal.workflow.kaleo.service.KaleoDraftDefinitionServiceUtil} to access the kaleo draft definition remote service.
+	 */
+}
