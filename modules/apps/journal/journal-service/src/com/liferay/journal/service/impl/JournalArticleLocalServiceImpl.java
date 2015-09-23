daf55677517ddc637f3e0f2cfdc994e06b64b121
Lino <lino.alves@liferay.com>
2015-09-23 17:34:25 -0300

    LPS-50660 -  Update latest version of DDMStructure

--- modules/apps/journal/journal-service/src/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
+++ modules/apps/journal/journal-service/src/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -7556,7 +7556,8 @@ public class JournalArticleLocalServiceImpl
 	}
 
 	protected void updateDDMStructurePredefinedValues(
-		long ddmStructureId, String content, ServiceContext serviceContext) {
+			long ddmStructureId, String content, ServiceContext serviceContext)
+		throws PortalException {
 
 		DDMStructure ddmStructure = ddmStructureLocalService.fetchDDMStructure(
 			ddmStructureId);
@@ -7591,9 +7592,9 @@ public class JournalArticleLocalServiceImpl
 			}
 		}
 
-		ddmStructure.updateDDMForm(ddmForm);
-
-		ddmStructureLocalService.updateDDMStructure(ddmStructure);
+		ddmStructureLocalService.updateStructure(
+				serviceContext.getUserId(), ddmStructureId, ddmForm,
+				ddmStructure.getDDMFormLayout(), serviceContext);
 	}
 
 	protected void updatePreviousApprovedArticle(JournalArticle article)
