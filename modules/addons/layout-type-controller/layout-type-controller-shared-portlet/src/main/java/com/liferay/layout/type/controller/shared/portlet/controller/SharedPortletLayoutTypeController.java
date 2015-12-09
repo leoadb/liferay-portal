f4e21a6289f09aba89bf47d0fe0e95921b0042b6
Leonardo Barros <leonardo.barros@liferay.com>
2015-12-09 14:34:50 -0300

    LPS-60963 Change properties of shared layout type

--- modules/addons/layout-type-controller/layout-type-controller-shared-portlet/src/main/java/com/liferay/layout/type/controller/shared/portlet/controller/SharedPortletLayoutTypeController.java
+++ modules/addons/layout-type-controller/layout-type-controller-shared-portlet/src/main/java/com/liferay/layout/type/controller/shared/portlet/controller/SharedPortletLayoutTypeController.java
@@ -42,7 +42,7 @@ public class SharedPortletLayoutTypeController
 
 	@Override
 	public String getType() {
-		return LayoutConstants.TYPE_PORTLET;
+		return LayoutConstants.TYPE_SHARED_PORTLET;
 	}
 
 	@Override
@@ -52,7 +52,7 @@ public class SharedPortletLayoutTypeController
 
 	@Override
 	public boolean isBrowsable() {
-		return true;
+		return false;
 	}
 
 	@Override
@@ -62,7 +62,7 @@ public class SharedPortletLayoutTypeController
 
 	@Override
 	public boolean isFullPageDisplayable() {
-		return true;
+		return false;
 	}
 
 	@Override
