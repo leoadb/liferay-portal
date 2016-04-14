520bd58a12b5610c9870e06a350bb5dcb217d0a3
Leonardo Barros <leonardo.barros@liferay.com>
2016-04-14 14:39:26 -0300

    Add display context for Field Library

---
+++ modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/java/com/liferay/dynamic/data/lists/form/web/display/context/DDLFormFieldLibraryDisplayContext.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.lists.form.web.display.context;
+
+import com.liferay.dynamic.data.lists.form.web.display.context.util.DDLFormFieldLibraryRequestHelper;
+import com.liferay.portal.kernel.util.ParamUtil;
+
+import javax.portlet.PortletURL;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+/**
+ * @author Leonardo Barros
+ */
+public class DDLFormFieldLibraryDisplayContext {
+
+	public DDLFormFieldLibraryDisplayContext(
+		RenderRequest renderRequest, RenderResponse renderResponse) {
+
+		_renderRequest = renderRequest;
+		_renderResponse = renderResponse;
+
+		_ddlFormFieldLibraryRequestHelper =
+			new DDLFormFieldLibraryRequestHelper(_renderRequest);
+	}
+
+	public PortletURL getPortletURL() {
+		PortletURL portletURL = _renderResponse.createRenderURL();
+
+		portletURL.setParameter("mvcPath", "/admin/view.jsp");
+		portletURL.setParameter(
+			"groupId",
+			String.valueOf(
+				_ddlFormFieldLibraryRequestHelper.getScopeGroupId()));
+		portletURL.setParameter(
+			"tab",
+			ParamUtil.getString(_renderRequest, "tab", "field-library"));
+
+		return portletURL;
+	}
+
+	private final DDLFormFieldLibraryRequestHelper
+		_ddlFormFieldLibraryRequestHelper;
+	private final RenderRequest _renderRequest;
+	private final RenderResponse _renderResponse;
+
+}
