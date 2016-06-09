18405f73e8c5ccd0ff52fe169812666a81935bfb
Leonardo Barros <leonardo.barros@liferay.com>
2016-06-09 08:36:27 -0300

    LPS-66426 Deprecate and create a new property

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/java/com/liferay/dynamic/data/mapping/form/field/type/DDMFormFieldTypeSettings.java
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/java/com/liferay/dynamic/data/mapping/form/field/type/DDMFormFieldTypeSettings.java
@@ -44,7 +44,9 @@ import com.liferay.dynamic.data.mapping.annotations.DDMFormLayoutRow;
 )
 public interface DDMFormFieldTypeSettings {
 
-	@DDMFormField(required = true, visibilityExpression = "FALSE")
+	@DDMFormField(
+		required = true, visibilityExpression = "FALSE"
+	)
 	public String dataType();
 
 	@DDMFormField(
