264ecb8d05f1b9acc807483ceac59ff764194f97
Leonardo Barros <leonardo.barros@liferay.com>
2016-06-30 09:37:44 -0300

    LPS-66919 Add properties to DDMFormRule

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/model/DDMFormRule.java
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/model/DDMFormRule.java
@@ -16,38 +16,84 @@ package com.liferay.dynamic.data.mapping.model;
 
 import java.io.Serializable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Leonardo Barros
  */
 public class DDMFormRule implements Serializable {
 
 	public DDMFormRule(DDMFormRule ddmFormRule) {
-		_expression = ddmFormRule._expression;
+		_condition = ddmFormRule._condition;
 		_ddmFormRuleType = ddmFormRule._ddmFormRuleType;
+		_enabled = ddmFormRule._enabled;
+		_message = ddmFormRule._message;
+
+		for (String action : ddmFormRule._actions) {
+			addAction(action);
+		}
 	}
 
-	public DDMFormRule(String expression, DDMFormRuleType ddmFormRuleType) {
-		_expression = expression;
+	public DDMFormRule(
+		List<String> actions, String condition, DDMFormRuleType ddmFormRuleType,
+		boolean enabled, String message) {
+
+		_actions = actions;
+		_condition = condition;
 		_ddmFormRuleType = ddmFormRuleType;
+		_enabled = enabled;
+		_message = message;
+	}
+
+	public void addAction(String action) {
+		_actions.add(action);
+	}
+
+	public List<String> getActions() {
+		return _actions;
+	}
+
+	public String getCondition() {
+		return _condition;
 	}
 
 	public DDMFormRuleType getDDMFormFieldRuleType() {
 		return _ddmFormRuleType;
 	}
 
-	public String getExpression() {
-		return _expression;
+	public String getMessage() {
+		return _message;
+	}
+
+	public boolean isEnabled() {
+		return _enabled;
+	}
+
+	public void setActions(List<String> actions) {
+		_actions = actions;
+	}
+
+	public void setCondition(String condition) {
+		_condition = condition;
 	}
 
 	public void setDDMFormRuleType(DDMFormRuleType ddmFormRuleType) {
 		_ddmFormRuleType = ddmFormRuleType;
 	}
 
-	public void setExpression(String expression) {
-		_expression = expression;
+	public void setEnabled(boolean enabled) {
+		_enabled = enabled;
 	}
 
+	public void setMessage(String message) {
+		_message = message;
+	}
+
+	private List<String> _actions = new ArrayList<>();
+	private String _condition;
 	private DDMFormRuleType _ddmFormRuleType;
-	private String _expression;
+	private boolean _enabled = true;
+	private String _message;
 
 }
