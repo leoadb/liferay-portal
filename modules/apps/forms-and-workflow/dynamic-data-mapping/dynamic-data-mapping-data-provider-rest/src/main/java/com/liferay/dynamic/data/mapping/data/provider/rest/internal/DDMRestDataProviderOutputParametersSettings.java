f88586aef5770bc4d7a5c7b8cfd7af9b269cbc20
Leonardo Barros <leonardo.barros@liferay.com>
2016-12-13 16:40:00 -0300

    LPS-69555 Apply layouts

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-data-provider-rest/src/main/java/com/liferay/dynamic/data/mapping/data/provider/rest/internal/DDMRestDataProviderOutputParametersSettings.java
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-data-provider-rest/src/main/java/com/liferay/dynamic/data/mapping/data/provider/rest/internal/DDMRestDataProviderOutputParametersSettings.java
@@ -16,11 +16,30 @@ package com.liferay.dynamic.data.mapping.data.provider.rest.internal;
 
 import com.liferay.dynamic.data.mapping.annotations.DDMForm;
 import com.liferay.dynamic.data.mapping.annotations.DDMFormField;
+import com.liferay.dynamic.data.mapping.annotations.DDMFormLayout;
+import com.liferay.dynamic.data.mapping.annotations.DDMFormLayoutColumn;
+import com.liferay.dynamic.data.mapping.annotations.DDMFormLayoutPage;
+import com.liferay.dynamic.data.mapping.annotations.DDMFormLayoutRow;
 
 /**
  * @author Leonardo Barros
  */
 @DDMForm
+@DDMFormLayout(
+	{
+		@DDMFormLayoutPage(
+			{
+				@DDMFormLayoutRow(
+					{
+						@DDMFormLayoutColumn(
+							size = 4, value = {"name", "path", "type"}
+						)
+					}
+				)
+			}
+		)
+	}
+)
 public interface DDMRestDataProviderOutputParametersSettings {
 
 	@DDMFormField(
