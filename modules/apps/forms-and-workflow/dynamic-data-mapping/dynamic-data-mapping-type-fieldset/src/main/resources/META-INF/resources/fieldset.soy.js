a80c195c52b01d517b931bd3bd0a39173a942b45
Leonardo Barros <leonardo.barros@liferay.com>
2016-12-14 13:25:55 -0300

    LPS-69555 buildSoy

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-type-fieldset/src/main/resources/META-INF/resources/fieldset.soy.js
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-type-fieldset/src/main/resources/META-INF/resources/fieldset.soy.js
@@ -21,9 +21,15 @@ soy.$$registerDelegateFn(soy.$$getDelTemplateId('ddm.field'), 'fieldset', 0, ddm
 
 
 ddm.fieldset_column = function(opt_data, opt_ignored) {
-  var output = '<div class="col-md-' + soy.$$escapeHtmlAttribute(opt_data.columnSize) + '">';
-  var variant__soy8 = opt_data.field.type;
-  output += soy.$$getDelegateFn(soy.$$getDelTemplateId('ddm.field'), variant__soy8, true)(opt_data.field) + '</div>';
+  var output = '';
+  var fieldList11 = opt_data.column.fields;
+  var fieldListLen11 = fieldList11.length;
+  for (var fieldIndex11 = 0; fieldIndex11 < fieldListLen11; fieldIndex11++) {
+    var fieldData11 = fieldList11[fieldIndex11];
+    output += '<div class="col-md-' + soy.$$escapeHtmlAttribute(opt_data.column.size) + '">';
+    var variant__soy8 = fieldData11.type;
+    output += soy.$$getDelegateFn(soy.$$getDelTemplateId('ddm.field'), variant__soy8, true)(fieldData11) + '</div>';
+  }
   return output;
 };
 if (goog.DEBUG) {
@@ -33,11 +39,11 @@ if (goog.DEBUG) {
 
 ddm.fieldset_columns = function(opt_data, opt_ignored) {
   var output = '';
-  var fieldList15 = opt_data.fields;
-  var fieldListLen15 = fieldList15.length;
-  for (var fieldIndex15 = 0; fieldIndex15 < fieldListLen15; fieldIndex15++) {
-    var fieldData15 = fieldList15[fieldIndex15];
-    output += ddm.fieldset_column(soy.$$augmentMap(opt_data, {columnSize: opt_data.columnSize, field: fieldData15}));
+  var columnList16 = opt_data.columns;
+  var columnListLen16 = columnList16.length;
+  for (var columnIndex16 = 0; columnIndex16 < columnListLen16; columnIndex16++) {
+    var columnData16 = columnList16[columnIndex16];
+    output += ddm.fieldset_column(soy.$$augmentMap(opt_data, {column: columnData16}));
   }
   return output;
 };
@@ -46,8 +52,23 @@ if (goog.DEBUG) {
 }
 
 
+ddm.fieldset_rows = function(opt_data, opt_ignored) {
+  var output = '';
+  var rowList23 = opt_data.rows;
+  var rowListLen23 = rowList23.length;
+  for (var rowIndex23 = 0; rowIndex23 < rowListLen23; rowIndex23++) {
+    var rowData23 = rowList23[rowIndex23];
+    output += '<div class="row">' + ddm.fieldset_columns(soy.$$augmentMap(opt_data, {columns: rowData23.columns})) + '</div>';
+  }
+  return output;
+};
+if (goog.DEBUG) {
+  ddm.fieldset_rows.soyTemplateName = 'ddm.fieldset_rows';
+}
+
+
 ddm.fieldset = function(opt_data, opt_ignored) {
-  return '<div class="form-group' + soy.$$escapeHtmlAttribute(opt_data.visible ? '' : ' hide') + ' liferay-ddm-form-field-fieldset" data-fieldname="' + soy.$$escapeHtmlAttribute(opt_data.name) + '">' + ((opt_data.tip) ? '<p class="liferay-ddm-form-field-tip">' + soy.$$escapeHtml(opt_data.tip) + '</p>' : '') + '<fieldset><legend>' + soy.$$escapeHtml(opt_data.label) + '</legend>' + ddm.fieldset_columns(opt_data) + '</fieldset></div>';
+  return '<div class="form-group' + soy.$$escapeHtmlAttribute(opt_data.visible ? '' : ' hide') + ' liferay-ddm-form-field-fieldset" data-fieldname="' + soy.$$escapeHtmlAttribute(opt_data.name) + '">' + ((opt_data.tip) ? '<p class="liferay-ddm-form-field-tip">' + soy.$$escapeHtml(opt_data.tip) + '</p>' : '') + '<fieldset>' + ((opt_data.showLabel) ? '<legend>' + soy.$$escapeHtml(opt_data.label) + '</legend>' : '') + ddm.fieldset_rows(opt_data) + '</fieldset></div>';
 };
 if (goog.DEBUG) {
   ddm.fieldset.soyTemplateName = 'ddm.fieldset';
