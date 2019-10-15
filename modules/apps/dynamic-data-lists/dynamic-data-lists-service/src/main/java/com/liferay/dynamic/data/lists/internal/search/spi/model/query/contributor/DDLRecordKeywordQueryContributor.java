76c8e12d05d8a4c1f2d533c77c0c079cf8bc433a
Lino Alves <lino.alves@liferay.com>
2019-10-15 16:08:57 -0300

    LPS-103222 search in all available locales

--- modules/apps/dynamic-data-lists/dynamic-data-lists-service/src/main/java/com/liferay/dynamic/data/lists/internal/search/spi/model/query/contributor/DDLRecordKeywordQueryContributor.java
+++ modules/apps/dynamic-data-lists/dynamic-data-lists-service/src/main/java/com/liferay/dynamic/data/lists/internal/search/spi/model/query/contributor/DDLRecordKeywordQueryContributor.java
@@ -16,6 +16,8 @@ package com.liferay.dynamic.data.lists.internal.search.spi.model.query.contribut
 
 import com.liferay.portal.kernel.search.BooleanQuery;
 import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.search.localization.SearchLocalizationHelper;
 import com.liferay.portal.search.query.QueryHelper;
 import com.liferay.portal.search.spi.model.query.contributor.KeywordQueryContributor;
 import com.liferay.portal.search.spi.model.query.contributor.helper.KeywordQueryContributorHelper;
@@ -39,16 +41,28 @@ public class DDLRecordKeywordQueryContributor
 		String keywords, BooleanQuery booleanQuery,
 		KeywordQueryContributorHelper keywordQueryContributorHelper) {
 
+		SearchContext searchContext =
+			keywordQueryContributorHelper.getSearchContext();
+
 		queryHelper.addSearchTerm(
 			booleanQuery, keywordQueryContributorHelper.getSearchContext(),
 			Field.USER_NAME, false);
 
-		queryHelper.addSearchLocalizedTerm(
-			booleanQuery, keywordQueryContributorHelper.getSearchContext(),
-			"ddmContent", false);
+		String[] localizedFieldNames =
+			_searchLocalizationHelper.getLocalizedFieldNames(
+				new String[] {"ddmContent"}, searchContext);
+
+		for (String localizedFieldName : localizedFieldNames) {
+			queryHelper.addSearchTerm(
+				booleanQuery, keywordQueryContributorHelper.getSearchContext(),
+				localizedFieldName, false);
+		}
 	}
 
 	@Reference
 	protected QueryHelper queryHelper;
+
+	@Reference
+	private SearchLocalizationHelper _searchLocalizationHelper;
 
 }
