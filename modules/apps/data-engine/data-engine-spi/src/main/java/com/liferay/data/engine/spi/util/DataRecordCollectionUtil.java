e646c65fff8aa9550a124a221a744dbd26283233
Jeyvison Nascimento <jeyvison.nascimento@liferay.com>
2020-02-14 16:22:33 -0300

    LPS-108395 Create Utils

---
+++ modules/apps/data-engine/data-engine-spi/src/main/java/com/liferay/data/engine/spi/util/DataRecordCollectionUtil.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.data.engine.spi.util;
+
+import com.liferay.data.engine.field.type.util.LocalizedValueUtil;
+import com.liferay.data.engine.spi.model.SPIDataRecordCollection;
+import com.liferay.dynamic.data.lists.model.DDLRecordSet;
+
+/**
+ * @author Jeyvison Nascimento
+ */
+public class DataRecordCollectionUtil {
+
+	public static SPIDataRecordCollection toDataRecordCollection(
+		DDLRecordSet ddlRecordSet) {
+
+		return new SPIDataRecordCollection() {
+			{
+				setDescription(
+					LocalizedValueUtil.toStringObjectMap(
+						ddlRecordSet.getDescriptionMap()));
+				setId(ddlRecordSet.getRecordSetId());
+				setName(
+					LocalizedValueUtil.toStringObjectMap(
+						ddlRecordSet.getNameMap()));
+				setSiteId(ddlRecordSet.getGroupId());
+				setSPIDataDefinitionId(ddlRecordSet.getDDMStructureId());
+				setSPIDataRecordCollectionKey(ddlRecordSet.getRecordSetKey());
+			}
+		};
+	}
+
+}
