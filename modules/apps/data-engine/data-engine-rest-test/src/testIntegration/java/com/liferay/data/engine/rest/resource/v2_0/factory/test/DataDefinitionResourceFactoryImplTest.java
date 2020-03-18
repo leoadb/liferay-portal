7d86bc4605eaa2453ddb34ec2873351e50f72442
Leonardo Barros <leonardo.barros@liferay.com>
2020-03-18 17:39:40 -0300

    LPS-110347 add tests

--- modules/apps/data-engine/data-engine-rest-test/src/testIntegration/java/com/liferay/data/engine/rest/resource/v2_0/factory/test/DataDefinitionResourceFactoryImplTest.java
+++ modules/apps/data-engine/data-engine-rest-test/src/testIntegration/java/com/liferay/data/engine/rest/resource/v2_0/factory/test/DataDefinitionResourceFactoryImplTest.java
@@ -16,10 +16,18 @@ package com.liferay.data.engine.rest.resource.v2_0.factory.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.data.engine.rest.resource.v2_0.DataDefinitionResource;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import javax.ws.rs.ClientErrorException;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -27,6 +35,8 @@ import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import org.springframework.mock.web.MockHttpServletRequest;
 
 /**
  * @author Brian Wing Shun Chan
@@ -45,17 +55,80 @@ public class DataDefinitionResourceFactoryImplTest {
 	}
 
 	@Test
-	public void testAcceptLanguage() throws Exception {
+	public void testAcceptLanguage1() throws Exception {
+		DataDefinitionResource dataDefinitionResource =
+			DataDefinitionResource.builder(
+			).httpServletRequest(
+				new MockHttpServletRequest() {
+
+					@Override
+					public String getHeader(String name) {
+						if (StringUtil.equals(name, "Accept-Language")) {
+							return "pt-BR";
+						}
+
+						return super.getHeader(name);
+					}
+
+				}
+			).user(
+				_user
+			).build();
+
+		JSONArray jsonArray = _jsonFactory.createJSONArray(
+			dataDefinitionResource.
+				getDataDefinitionDataDefinitionFieldFieldTypes());
+
+		for (int i = 0; i < jsonArray.length(); i++) {
+			JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+			Assert.assertEquals(
+				"pt_BR",
+				JSONUtil.getValue(
+					jsonObject, "JSONObject/settingsContext", "JSONArray/pages",
+					"Object/0", "JSONArray/rows", "Object/0",
+					"JSONArray/columns", "Object/0", "JSONArray/fields",
+					"Object/0", "Object/locale"));
+		}
+	}
+
+	@Test(expected = ClientErrorException.class)
+	public void testAcceptLanguage2() throws Exception {
+		DataDefinitionResource dataDefinitionResource =
+			DataDefinitionResource.builder(
+			).httpServletRequest(
+				new MockHttpServletRequest() {
+
+					@Override
+					public String getHeader(String name) {
+						if (StringUtil.equals(name, "Accept-Language")) {
+							return "INVALID";
+						}
+
+						return super.getHeader(name);
+					}
+
+				}
+			).user(
+				_user
+			).build();
+
+		dataDefinitionResource.getDataDefinitionDataDefinitionFieldFieldTypes();
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void testHttpServletRequest() throws Exception {
 		DataDefinitionResource dataDefinitionResource =
 			DataDefinitionResource.builder(
 			).user(
 				_user
 			).build();
 
-		Assert.assertNotNull(
-			dataDefinitionResource.
-				getDataDefinitionDataDefinitionFieldFieldTypes());
+		dataDefinitionResource.getDataDefinitionDataDefinitionFieldFieldTypes();
 	}
+
+	@Inject(type = JSONFactory.class)
+	private JSONFactory _jsonFactory;
 
 	@DeleteAfterTestRun
 	private User _user;
