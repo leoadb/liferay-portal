ae1a65fd4f153159c5ee26f2de4cf1bcfe916d87
Leonardo Barros <leonardo.barros@liferay.com>
2020-03-18 07:57:51 -0300

    LPS-110347 add tests

--- modules/apps/data-engine/data-engine-rest-test/src/testIntegration/java/com/liferay/data/engine/rest/resource/v2_0/factory/test/DataDefinitionResourceFactoryImplTest.java
+++ modules/apps/data-engine/data-engine-rest-test/src/testIntegration/java/com/liferay/data/engine/rest/resource/v2_0/factory/test/DataDefinitionResourceFactoryImplTest.java
@@ -19,7 +19,11 @@ import com.liferay.data.engine.rest.resource.v2_0.DataDefinitionResource;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import java.util.Locale;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -27,6 +31,8 @@ import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import org.springframework.mock.web.MockHttpServletRequest;
 
 /**
  * @author Brian Wing Shun Chan
@@ -45,16 +51,72 @@ public class DataDefinitionResourceFactoryImplTest {
 	}
 
 	@Test
-	public void testAcceptLanguage() throws Exception {
+	public void testAcceptLanguage1() throws Exception {
+		_user.setLanguageId("en_US");
+
+		DataDefinitionResource dataDefinitionResource =
+			DataDefinitionResource.builder(
+			).httpServletRequest(
+				new MockHttpServletRequest() {
+
+					@Override
+					public Locale getLocale() {
+						return LocaleUtil.BRAZIL;
+					}
+
+				}
+			).user(
+				_user
+			).build();
+
+		String fieldTypes =
+			dataDefinitionResource.
+				getDataDefinitionDataDefinitionFieldFieldTypes();
+
+		Assert.assertTrue(
+			StringUtil.contains(fieldTypes, "\"locale\":\"pt_BR\""));
+		Assert.assertFalse(
+			StringUtil.contains(fieldTypes, "\"locale\":\"en_US\""));
+	}
+
+	@Test
+	public void testAcceptLanguage2() throws Exception {
+		_user.setLanguageId("en_US");
+
+		DataDefinitionResource dataDefinitionResource =
+			DataDefinitionResource.builder(
+			).httpServletRequest(
+				new MockHttpServletRequest() {
+
+					@Override
+					public Locale getLocale() {
+						return null;
+					}
+
+				}
+			).user(
+				_user
+			).build();
+
+		String fieldTypes =
+			dataDefinitionResource.
+				getDataDefinitionDataDefinitionFieldFieldTypes();
+
+		Assert.assertTrue(
+			StringUtil.contains(fieldTypes, "\"locale\":\"en_US\""));
+		Assert.assertFalse(
+			StringUtil.contains(fieldTypes, "\"locale\":\"pt_BR\""));
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void testHttpServletRequest() throws Exception {
 		DataDefinitionResource dataDefinitionResource =
 			DataDefinitionResource.builder(
 			).user(
 				_user
 			).build();
 
-		Assert.assertNotNull(
-			dataDefinitionResource.
-				getDataDefinitionDataDefinitionFieldFieldTypes());
+		dataDefinitionResource.getDataDefinitionDataDefinitionFieldFieldTypes();
 	}
 
 	@DeleteAfterTestRun
