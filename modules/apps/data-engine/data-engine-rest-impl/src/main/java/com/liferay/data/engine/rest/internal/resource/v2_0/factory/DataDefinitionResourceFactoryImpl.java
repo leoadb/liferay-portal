8d80d97705f99f35e104ad67d197347b88581812
Leonardo Barros <leonardo.barros@liferay.com>
2020-03-18 16:13:13 -0300

    LPS-110347 copied from portal-vulcan-impl

--- modules/apps/data-engine/data-engine-rest-impl/src/main/java/com/liferay/data/engine/rest/internal/resource/v2_0/factory/DataDefinitionResourceFactoryImpl.java
+++ modules/apps/data-engine/data-engine-rest-impl/src/main/java/com/liferay/data/engine/rest/internal/resource/v2_0/factory/DataDefinitionResourceFactoryImpl.java
@@ -15,6 +15,9 @@
 package com.liferay.data.engine.rest.internal.resource.v2_0.factory;
 
 import com.liferay.data.engine.rest.resource.v2_0.DataDefinitionResource;
+import com.liferay.portal.kernel.exception.NoSuchUserException;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
@@ -23,8 +26,10 @@ import com.liferay.portal.kernel.security.permission.PermissionCheckerFactory;
 import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
 import com.liferay.portal.kernel.service.CompanyLocalService;
 import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.ProxyUtil;
 import com.liferay.portal.vulcan.accept.language.AcceptLanguage;
 
@@ -34,10 +39,15 @@ import java.lang.reflect.Method;
 import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
+import java.util.Set;
 
 import javax.annotation.Generated;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.ClientErrorException;
+import javax.ws.rs.InternalServerErrorException;
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.core.HttpHeaders;
 
 import org.osgi.service.component.ComponentServiceObjects;
 import org.osgi.service.component.annotations.Activate;
@@ -140,7 +150,7 @@ public class DataDefinitionResourceFactoryImpl
 			_componentServiceObjects.getService();
 
 		dataDefinitionResource.setContextAcceptLanguage(
-			new AcceptLanguageImpl(user));
+			new AcceptLanguageImpl(httpServletRequest));
 
 		Company company = _companyLocalService.getCompany(user.getCompanyId());
 
@@ -180,15 +190,58 @@ public class DataDefinitionResourceFactoryImpl
 	@Reference
 	private UserLocalService _userLocalService;
 
+	@Reference
+	private Language _language;
+
+	@Reference
+	private Portal _portal;
+
 	private class AcceptLanguageImpl implements AcceptLanguage {
 
-		public AcceptLanguageImpl(User user) {
-			_user = user;
+		public AcceptLanguageImpl(HttpServletRequest httpServletRequest) {
+			_httpServletRequest = httpServletRequest;
 		}
 
 		@Override
 		public List<Locale> getLocales() {
-			return Collections.emptyList();
+			String acceptLanguage = _httpServletRequest.getHeader(
+				HttpHeaders.ACCEPT_LANGUAGE);
+
+			if (acceptLanguage == null) {
+				return Collections.emptyList();
+			}
+
+			if (acceptLanguage.equals("zh-Hans-CN")) {
+				acceptLanguage = "zh-CN";
+			}
+			else if (acceptLanguage.equals("zh-Hant-TW")) {
+				acceptLanguage = "zh-TW";
+			}
+
+			try {
+				Company company = _portal.getCompany(_httpServletRequest);
+
+				Set<Locale> companyAvailableLocales =
+					_language.getCompanyAvailableLocales(company.getCompanyId());
+
+				List<Locale> locales = Locale.filter(
+					Locale.LanguageRange.parse(acceptLanguage),
+					companyAvailableLocales);
+
+				if (ListUtil.isEmpty(locales)) {
+					throw new ClientErrorException(
+						"No locales match the accepted languages: " +
+						acceptLanguage,
+						422);
+				}
+
+				return locales;
+			}
+			catch (PortalException portalException) {
+				throw new InternalServerErrorException(
+					"Unable to get locales: " + portalException.getMessage(),
+					portalException);
+			}
 		}
 
 		@Override
@@ -204,15 +257,30 @@ public class DataDefinitionResourceFactoryImpl
 				return locales.get(0);
 			}
 
-			return _user.getLocale();
+			try {
+				User user = _portal.initUser(_httpServletRequest);
+
+				return user.getLocale();
+			}
+			catch (NoSuchUserException noSuchUserException) {
+				throw new NotFoundException(
+					"Unable to get preferred locale from nonexistent user",
+					noSuchUserException);
+			}
+			catch (Exception exception) {
+				throw new InternalServerErrorException(
+					"Unable to get preferred locale: " + exception.getMessage(),
+					exception);
+			}
 		}
 
 		@Override
 		public boolean isAcceptAllLanguages() {
-			return false;
+			return GetterUtil.getBoolean(
+				_httpServletRequest.getHeader("X-Accept-All-Languages"));
 		}
 
-		private final User _user;
+		private final HttpServletRequest _httpServletRequest;
 
 	}
 
